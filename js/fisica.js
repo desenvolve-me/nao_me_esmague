98e1a3d8a174701289735283c79cf48b1d064818
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2019-10-03 22:09:45 -0300

    Adiciona personagem controlado via teclado

--- js/fisica.js
+++ js/fisica.js
@@ -1,7 +1,7 @@
 let personagem = document.getElementById("personagem");
 let chao = document.getElementById("chao");
-
 var aceleracao = 1
+var correr_com_personagem = correr_para_direita;
 
 function desenha_chao() {
     tamanho_chao = 0;
@@ -20,11 +20,48 @@ function desenha_chao() {
     }
 }
 
-function loop(){ 
+function loop() {
+    correr_com_personagem(8)
     window.requestAnimationFrame(loop);
 }
 
+function correr_para_direita (velocidade) {
+    posicao_do_personagem = Number(personagem.style.left.replace("px", "")) + 1            
+    fim_da_tela = (document.documentElement.clientWidth - personagem.offsetWidth)
+
+    personagem.style.transform = "scaleX(1)"
+
+    if(posicao_do_personagem < fim_da_tela) {
+        personagem.style.left = (posicao_do_personagem + velocidade);  
+    }
+}
+
+function correr_para_esquerda (velocidade) {
+    posicao_do_personagem = Number(personagem.style.left.replace("px", "")) + 1            
+    fim_da_tela = 0
+
+    personagem.style.transform = "scaleX(-1)"
+
+    if(posicao_do_personagem > fim_da_tela) {
+        personagem.style.left = (posicao_do_personagem - velocidade);  
+    }
+}
+
+document.addEventListener("keydown", function(e){
+    switch(e.keyCode){
+        //press left key
+        case 37:
+            correr_com_personagem = correr_para_esquerda;
+            break;
+
+        //press right key
+        case 39:
+            correr_com_personagem = correr_para_direita;
+            break;
+    }
+    
+});
+
+
 desenha_chao();
 loop();
-
-//file:///Users/macbook/dev/nao_me_esmague/jogo.html
